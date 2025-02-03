6659519a16bc6c4876806f6e60f9406fd0e4841b
ferreiram <mathilde.ferreira@meteo.fr>
2025-02-03 15:06:02 +0000

    reformat

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -2,14 +2,7 @@ from pathlib import Path
 
 from langchain_core.documents.base import Document
 
-from main import (
-    answer,
-    create_bm25_idx,
-    create_chroma_db,
-    get_db_path,
-    query,
-    rerank,
-)
+from main import answer, create_bm25_idx, create_chroma_db, get_db_path, query, rerank
 from rag_drias.crawler import crawl_website
 from rag_drias.data import filter_similar_chunks
 from rag_drias.embedding import get_embedding
